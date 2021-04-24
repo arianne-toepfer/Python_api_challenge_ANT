a0d973925e9cea1d0788cd952e8dcbf58487bd47
arianne-toepfer <78578889+arianne-toepfer@users.noreply.github.com>
2021-04-23 17:06:30 -0700

    updates

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "175624b432a0f7ff5c0920a58053851d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCU1c4XYs8clrwtecp7OTaqAH1n9MdywKk"
